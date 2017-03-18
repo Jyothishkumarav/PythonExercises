43a4f927d7fec7e3c4ee9b1f8bf4017946b58aae
jyothishkumarav <jyothishkumarav@gmail.com>
2017-03-18 12:35:12 +0530

    odd even sample program

---
+++ odeven.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 10:42:27 2017
+
+@author: 310251316
+"""
+
+number = int(input("Enter a number:"))
+mod = number % 2
+if mod == 0 :
+    print("%d is an Even number"%number)
+else:
+    print("%d is an Odd number"%number)
